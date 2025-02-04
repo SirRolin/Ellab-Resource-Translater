6eb41c7e93cfaf3286f7a588bf3bd9a31c716183
Nicolai Theis Rolin <ntr@ellab.com>
2025-02-04 14:27:33 +0100

    Saved Debugging Sql

---
+++ TranslationsSqls.sql
@@ -0,0 +1,20 @@
+﻿
+/* See EMSuite entries */
+/*select * from Translation where SystemEnum = 1;*/
+
+
+/* See Valsuite entries */
+/*select * from Translation where SystemEnum = 0;*/
+
+/* Delete Duplicates */
+/*
+WITH added_row_number AS (
+  SELECT
+    *,
+    ROW_NUMBER() OVER(PARTITION BY "Key" + LanguageCode + ResourceName ORDER BY ID ASC) AS row_number
+  FROM Translation
+)
+SELECT
+  *
+FROM added_row_number
+WHERE row_number <> 1;*/
